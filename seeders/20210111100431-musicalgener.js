3995cb418ad17e3edb92e0e9be166558589fbc97
filmons <filmonseare@gmail.com>
2021-07-01 12:36:06 +0200

    confugretion and db seeds squelize

---
+++ seeders/20210111100431-musicalgener.js
@@ -0,0 +1,12 @@
+//const musicalGenres = require("../seeds/20210111100431-musical_genres");
+const musicalGenres = require("../config/seeds/20210111100431-musical_genres");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("Musicalgenres", musicalGenres, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Musicalgenres", null, {});
+  },
+};
